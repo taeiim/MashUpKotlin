7ce9471df745972eeaa91d80a3668076c795101e
taeiim <taeiim.god@gmail.com>
2020-04-04 00:23:23 +0900

    MainActivity 추가

---
+++ GitToy/app/src/main/java/com/taeiim/gittoy/ui/main/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.taeiim.gittoy.ui.main
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.taeiim.gittoy.BR
+import com.taeiim.gittoy.R
+import com.taeiim.gittoy.api.model.GithubRepo
+import com.taeiim.gittoy.base.BaseActivity
+import com.taeiim.gittoy.databinding.ActivityMainBinding
+import com.taeiim.gittoy.databinding.ItemRepoBinding
+import com.taeiim.gittoy.ext.start
+import com.taeiim.gittoy.ui.RepoRecyclerAdapter
+import com.taeiim.gittoy.ui.search.SearchActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private val vm: MainViewModel by viewModel()
+    lateinit var repoAdapter: RepoRecyclerAdapter<GithubRepo, ItemRepoBinding>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.vm = vm
+
+        repoAdapter = RepoRecyclerAdapter(this@MainActivity, false, R.layout.item_repo, BR.repo)
+        binding.historyRepoRv.adapter = repoAdapter
+
+        vm.setObserves()
+        updateHistoryItems()
+
+    }
+
+    private fun MainViewModel.setObserves() {
+        repoHistoryList.observe(this@MainActivity, Observer { updateItems(it) })
+        startSearchActivityEvent.observe(this@MainActivity, Observer { goSearchActivity() })
+        errorQueryBlank.observe(this@MainActivity, Observer { blankSearchQuery() })
+    }
+
+    private fun updateHistoryItems() {
+        vm.getRepoHistory()
+    }
+
+    private fun updateItems(resultList: List<GithubRepo>) {
+        repoAdapter.updateItems(resultList)
+    }
+
+    private fun blankSearchQuery() {
+        Toast.makeText(this, getString(R.string.blank_search), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goSearchActivity() {
+        start(SearchActivity::class) {
+            putString("searchWord", vm.query.value)
+        }
+        binding.searchEditTv.text.clear()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        vm.getRepoHistory()
+    }
+
+}
