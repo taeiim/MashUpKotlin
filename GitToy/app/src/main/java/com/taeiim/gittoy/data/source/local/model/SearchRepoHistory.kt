96ed6dd4ba3b5e68142fe3e461aad4020c135fa6
taeiim <taeiim.dev@gmail.com>
2020-05-10 16:09:15 +0900

    ColumnInfo 제거

--- GitToy/app/src/main/java/com/taeiim/gittoy/data/source/local/model/SearchRepoHistory.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/data/source/local/model/SearchRepoHistory.kt
@@ -1,12 +1,13 @@
 package com.taeiim.gittoy.data.source.local.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.taeiim.gittoy.api.model.GithubRepo
 
 @Entity(tableName = "search_repo_history")
 data class SearchRepoHistory(
-    @ColumnInfo(name = "repo") val repo: GithubRepo,
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int = 0
+    val repo: GithubRepo,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
 )
