2acb8d5a839fe08d08adca53bce297f55287f26d
taeiim <taeiim.dev@gmail.com>
2020-05-11 02:21:01 +0900

    @Embedded 추가

--- GitToy/app/src/main/java/com/taeiim/gittoy/data/source/local/model/SearchRepoHistory.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/data/source/local/model/SearchRepoHistory.kt
@@ -1,11 +1,13 @@
 package com.taeiim.gittoy.data.source.local.model
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.taeiim.gittoy.api.model.GithubRepo
 
 @Entity(tableName = "search_repo_history")
 data class SearchRepoHistory(
+    @Embedded
     val repo: GithubRepo,
 
     @PrimaryKey(autoGenerate = true)
