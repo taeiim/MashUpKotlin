e8911961665e7f145e92b999ba8c9ce27f57001e
taeiim <taeiim.god@gmail.com>
2020-05-13 14:48:24 +0900

    stetho 추가

--- GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
@@ -1,6 +1,7 @@
 package com.taeiim.gittoy
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.taeiim.gittoy.di.dataSourceModule
 import com.taeiim.gittoy.di.networkModule
 import com.taeiim.gittoy.di.viewModelModule
@@ -11,6 +12,7 @@ class GitToyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         setupKoin(this, viewModelModule, dataSourceModule, networkModule)
+        Stetho.initializeWithDefaults(this)
     }
 
 }
